2e096523a663c1bcffee397606641e43227d9d7e
sukeshbirwa123 <134356011+sukeshbirwa123@users.noreply.github.com>
2023-08-28 15:45:17 +0530

    Update arith.py

--- python program/arith.py
+++ python program/arith.py
@@ -1,13 +1,1 @@
-def add(n1,n2):
-    return(n1+n2)
-def sub(n1,n2):
-    return(n1-n2)
-n1=int(input("Enter first number: "))
-n2=int(input("Enter second number: "))
-print("Sum= ",add(n1,n2))
-print("Sum= ",sub(n1,n2))
-def mul(n1,n2):
-    return(n1*n2)
-n1=int(input("Enter first number: "))
-n2=int(input("Enter second number: "))
-print("Sum= ",add(n1,n2))
+
